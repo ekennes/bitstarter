c3b2ebf4f031a9cf306b819615bfd7532a52a98e
ekennes <erik.kennes@gmail.com>
2013-07-11 20:40:06 -0400

    Updated wed.js

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var buffer = new Buffer(20)
 fs.readFileSync('/bitstarter/bitstarter/index.html', buffer)
 
 app.get('/', function(request, response) {
-  response.send(buffer);
+  response.send(buffer.tostring('utf-8');
 });
 
 var port = process.env.PORT || 5000;
